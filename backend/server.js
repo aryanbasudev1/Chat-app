06dcd607c6ffb86194c601683570d416912aafd9
aryanbasudev1 <aryanbasudev1@gmail.com>
2024-06-15 16:34:24 +0530

    Backend done except SocketIO

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.routes.js"
 import messageRoutes from "./routes/message.routes.js"
+import userRoutes from "./routes/user.routes.js"
 import connectMongoDB from "./db/connectMongoDB.js";
 
 const app = express();
@@ -15,6 +17,7 @@ app.use(cookieParser())
 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
+app.use("/api/users", userRoutes)
 
 // app.get('/', (req, res) => {
 //     res.send('Hello World!!')
